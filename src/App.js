5896ad834d8ba6bc09d807eb5cf5508692416b06
Manoj-Krishna-Chandragiri <manojkrishnachandragiri@gmail.com>
2025-03-02 02:17:35 +0530

    Refactored API calls in App.js, updated package dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [fact, setFact] = useState("");
+  const fetchClick = () => {
+    fetchFact()
+  }
+  const axiosClick = () => {
+      fetchFact1()
+    }
+  const fetchFact = () => {
+  fetch("https://catfact.ninja/fact")
+    .then((response) => response.json())
+    .then((data) => setFact(data.fact));
 }
 
+const fetchFact1 = () => {
+    axios.get("https://catfact.ninja/fact").then((response) => {
+    setFact(response.data.fact)
+    });
+  }
+useEffect(() => {
+  fetchFact()
+}, []);
+return (
+  <div className="App">
+    <h2>Press the button for a random cat fact!</h2>
+    <hr />
+    <button onClick= {() => fetchClick()}>Get Cat fact using fetch</button>&nbsp;&nbsp;&nbsp;
+    <button onClick= {() => axiosClick()}>Get Cat fact using axios</button>
+    <p>{fact}</p>
+  </div>
+);
+}
 export default App;
